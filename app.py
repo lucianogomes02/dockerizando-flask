c9aebaf38c6eaf6a682945103d9e0101998d0576
lucianogomes02 <luciano@pontotel.com.br>
2022-08-05 09:37:52 -0300

    cadastrando Usuario

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from usuario import Usuario, Base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,6 +11,38 @@ engine = create_engine("postgresql+psycopg2://test:test@postgresql/test", echo=T
 Base.metadata.create_all(engine)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def ola_mundo():
     return {"ola": "mundo"}
+
+
+@app.route('/usuario/cadastro', methods=["POST"])
+def cadastrar_usuario():
+    if novo_usuario_eh_valido(dict(request.json)):
+        novo_usuario = Usuario(
+            nome=request.json.get("nome"),
+            criado_em=datetime.now()
+        )
+        with Session(engine) as session:
+            try:
+                session.add(novo_usuario)
+                session.commit()
+                session.close()
+                return jsonify(
+                    {
+                        "sucesso": f"Usuario cadastrado com sucesso!"
+                    }
+                )
+            except Exception:
+                session.rollback()
+    return jsonify(
+        {
+            "erro" : f"Nome de usuário inválido para cadastro. Verifique o body request!"
+        }
+    )
+
+
+def novo_usuario_eh_valido(novo_usuario: dict):
+    if "nome" in novo_usuario.keys() and novo_usuario.get("nome", ""):
+        return True
+    return False
