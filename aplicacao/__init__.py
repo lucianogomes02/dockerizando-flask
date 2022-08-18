2b85959965bff70c63ac62aa992f382385b2cc69
Luciano Gomes <luciano@pontotel.com.br>
2022-08-18 00:35:15 -0300

    refactor(aplicacao/__init__.py): alterando inicialização da aplicação para a função create_app

--- aplicacao/__init__.py
+++ aplicacao/__init__.py
@@ -1,3 +1,19 @@
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+def create_app(test_config=None):
+    from flask import Flask
+    from usuario.apis import usuario, spec
+    from usuario.migrations import usuario_migration
+    from .db import engine
+
+    app = Flask(__name__)
+
+    app.register_blueprint(usuario)
+    spec.register(app)
+
+    usuario_migration.create(engine, checkfirst=True)
+
+    return app
